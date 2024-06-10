35100f6be194c2412d579d8c4bb0969b3186876a
Yunus Shaikh <yunusshaikh29082@gmail.com>
2024-06-10 18:57:08 +0530

    added chat header

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { ChatHeader } from "@/components/chat/chat-header";
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
 
-const ChannelIdPage = () => {
-  return (
-    <div>channel ID page</div>
-  )
+interface ChannelIdPageProps {
+  params: {
+    channelId: string;
+    serverId: string;
+  };
 }
 
-export default ChannelIdPage
+const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
+  const profile = await currentProfile();
+
+  const channel = await db.channel.findUnique({
+    where: {
+      id: params.channelId,
+    },
+  });
+
+  const member = await db.member.findFirst({
+    where: {
+      serverId: params.serverId,
+      profileId: profile?.id,
+    },
+  });
+
+  if (!channel || !member) {
+    return redirect("/");
+  }
+
+  return (
+    <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+      <ChatHeader
+        name={channel.name}
+        serverId={params.serverId}
+        type="channel"
+      />
+    </div>
+  );
+};
+
+export default ChannelIdPage;
